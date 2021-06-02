807cb61c7c0d4b0a90a4d4cf7c6482302bd83a50
Arif <hossaininfo@ymail.com>
2021-06-02 16:54:11 +0200

    Thread handled

--- client/src/main/java/client/Client.java
+++ client/src/main/java/client/Client.java
@@ -1,7 +1,16 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        try {
+            Socket socket = new Socket("192.168.1.155", 5050);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- core/src/main/java/core/Main.java
+++ core/src/main/java/core/Main.java
@@ -4,25 +4,39 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
 
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
         try (ServerSocket serverSocket = new ServerSocket(5050)){
             while (true){
                 Socket client = serverSocket.accept();
-                System.out.println(client.getInetAddress());
-                PrintWriter outputToClient = new PrintWriter(client.getOutputStream());
-                outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+                executorService.submit(()->handleConnection(client));
+                output(client);
 
-                outputToClient.flush();
-                outputToClient.close();
                 client.close();
-
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void output(Socket client) throws IOException {
+
+        PrintWriter outputToClient = new PrintWriter(client.getOutputStream());
+        outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+
+        outputToClient.flush();
+        outputToClient.close();
+
+    }
+
+    private static void handleConnection(Socket client) {
+        System.out.println(client.getInetAddress());
     }
 }
