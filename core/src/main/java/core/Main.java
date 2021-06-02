150b0c0371c9cbea4ffc54c3dd45c82f68a2f5fc
Arif <hossaininfo@ymail.com>
2021-06-02 15:54:56 +0200

    created basic server

--- core/src/main/java/core/Main.java
+++ core/src/main/java/core/Main.java
@@ -1,7 +1,28 @@
 package core;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello again");
+
+        try (ServerSocket serverSocket = new ServerSocket(5050)){
+            while (true){
+                Socket client = serverSocket.accept();
+                System.out.println(client.getInetAddress());
+                PrintWriter outputToClient = new PrintWriter(client.getOutputStream());
+                outputToClient.print("HTTP/1.1 404 Not Found\r\nContent-length: 0\r\n\r\n");
+
+                outputToClient.flush();
+                outputToClient.close();
+                client.close();
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
